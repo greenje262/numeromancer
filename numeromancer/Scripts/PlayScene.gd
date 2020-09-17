0baa4ea8b88e0243d8211de1759b46728289c534
greenje <greenje262@gmail.com>
2020-09-16 23:31:49 -0400

    Added final sprites, exported final builds, and created promo images

--- numeromancer/Scripts/PlayScene.gd
+++ numeromancer/Scripts/PlayScene.gd
@@ -9,6 +9,8 @@ var right = rng.randi_range(0, 20)
 
 onready var score = 0
 onready var tween = $Numeromancer/Tween
+onready var game_timer = $GameTimer
+onready var clock = $TextureProgress
 
 func _ready():
 	game_prep()
@@ -21,6 +23,7 @@ func game_prep():
 	$Incorrect.hide()
 	$InputBox.hide()
 	$InputBox.editable = true
+	$Summons.visible = false
 	$Problem.hide()
 	$ResetButton.hide()
 	$Title.show()
@@ -39,13 +42,18 @@ func game_start():
 	
 	$InputBox.show()
 	$InputBox.grab_focus()
+	$TextureProgress.show()
 	rng.randomize()
 	math_problem()
 	
-	yield(get_tree().create_timer(10), "timeout")
+	game_timer.start()
+	game_timer.wait_time = clock.value
+	yield(game_timer, "timeout")
 	game_over()
 
 func _process(delta):
+	clock.value = game_timer.time_left
+	
 	if Input.is_action_just_pressed("answer") and $InputBox.text != "":
 		evaluate()
 
@@ -94,8 +102,10 @@ func game_over():
 	$Problem.text = ""
 	$InputBox.hide()
 	$InputBox.editable = false
+	$InputBox.clear()
 	$Correct.hide()
 	$Incorrect.hide()
+	$TextureProgress.hide()
 	summon()
 
 func summon():
@@ -110,14 +120,58 @@ func num_summon():
 
 func sum_summon():
 	$Numeromancer.play("release")
-	$Summons.play("poof")
-	$Problem.text = "nice!!"
+	summon_reveal()
 	$ResetButton.show()
 	$ResetButton.grab_focus()
 
 func _on_ResetButton_pressed():
+	$Problem.hide()
+	$ResetButton.hide()
+	$Summons.visible = false
 	$Numeromancer.play("idle")
 	tween.interpolate_property($Numeromancer, "position", Vector2(48, 32), Vector2(64, 32), 2)
 	tween.start()
 	yield(get_tree().create_timer(2), "timeout")
 	game_prep()
+
+func summon_reveal():
+	$Poof.set_frame(0)
+	$Poof.play("poof")
+	$Poof.set_frame(0)
+	yield(get_tree().create_timer(0.5), "timeout")
+	summon_picker()
+
+func summon_picker():
+	if score > 0:
+		$Summons.visible = true
+		$Problem.text = "nice!!"
+		if score < 5:
+			$Summons.play("animal01")
+		elif score >= 5:
+			if score < 10:
+				$Summons.play("animal02")
+			elif score >= 10:
+				if score < 15:
+					$Summons.play("animal03")
+				elif score >= 15:
+					if score < 20:
+						$Summons.play("animal04")
+					elif score >= 20:
+						if score < 25:
+							$Summons.play("animal05")
+						elif score >= 25:
+							if score < 30:
+								$Summons.play("animal06")
+							elif score >= 30:
+								if score < 35:
+									$Summons.play("animal07")
+								elif score >= 35:
+									if score < 40:
+										$Summons.play("animal08")
+									elif score >= 40:
+										if score < 45:
+											$Summons.play("animal09")
+										elif score >= 45:
+											$Summons.play("animal10")
+	else:
+		$Problem.text = "yikes!"
